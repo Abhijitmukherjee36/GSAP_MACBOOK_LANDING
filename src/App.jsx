bda1f5f532a8e5e33a6a422dfb284b0bd4518203
Abhijitmukherjee36 <abhijitmukherjee416@gmail.com>
2025-12-15 14:28:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import NavBar from "./components/NavBar"
+import { navLinks } from "./constants"
+const App = () => {
+  return (
+      <header>
+        <nav>
+          <img src="/logo.svg" alt="Apple logo" />
+
+          <ul>
+            {navLinks.map((link)=>(
+              <li key={link.label}>
+                <a href="{link.label}">{link.label}</a>
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex-center gap-3">
+            <button>
+              <img src="/search.svg" alt="Search" />
+            </button>
+            <button>
+              <img src="/cart.svg" alt="Cart" />
+            </button>
+          </div>
+        </nav>
+      </header>
+  )
+}
+
+export default App
